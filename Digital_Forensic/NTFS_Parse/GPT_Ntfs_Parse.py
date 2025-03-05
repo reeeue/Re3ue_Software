44b24508c885af1a5866d3bec70a0353d6267c12
reeeue 2024 <reeeue2024@icloud.com>
2025-03-05 13:40:30 +0900

    [ Digital Forensic ] GPT_NTFS_Parse : File List

--- Digital_Forensic/NTFS_Parse/GPT_Ntfs_Parse.py
+++ Digital_Forensic/NTFS_Parse/GPT_Ntfs_Parse.py
@@ -17,9 +17,11 @@ class NTFSParse :
     def __init__(self, file_path) :
         self.file_path = file_path
 
+        # NTFS - VBR
         self.bytes_per_sector = 0
         self.sector_per_cluster = 0
 
+        # NTFS - VBR
         self.partition_start_sector = 0
         self.partition_start_offset = 0
         self.mft_start_sector = 0
@@ -144,8 +146,8 @@ class NTFSParse :
                     "Index" : index,
                     "Partition Type GUID" : partition_type_guid,
                     "Unique Partition GUID" : unique_partition_guid,
-                    "First LBA" : starting_lba,
-                    "Last LBA" : ending_lba,
+                    "Starting LBA" : starting_lba,
+                    "Ending LBA" : ending_lba,
                     "Attribute Flags" : attribute_flags,
                     "Partition Name" : partition_name,
                     "Partition Size" : partition_size
@@ -172,17 +174,277 @@ class NTFSParse :
         print("\n========================================")
 
     """
+    NTFS - VBR
+    """
+    def get_vbr(self, f, partition) :
+        partition_starting_offset = partition.get("Starting LBA")
+        partition_ending_offset = partition.get("Ending LBA")
+
+        f.seek(partition_starting_offset * SECTOR_SIZE)
+
+        vbr_data = f.read(SECTOR_SIZE)
+
+        jump_boot_code = vbr_data[0:3] # 3 Bytes
+        oem_id = vbr_data[3:11] # 8 Bytes
+        bpb_data = vbr_data[11:84] # 73 Bytes
+        boot_code = vbr_data[84:510] # 426 Bytes
+        vbr_signature = vbr_data[510:512] # 2 Bytes
+
+        if oem_id != NTFS_OEM_ID :
+            return False
+        
+        if vbr_signature != VBR_SIGNATURE :
+            print("\n[ ERROR ] FAIL - VBR Signature ( 55 AA )")
+            print(f">>>>>>>>>> File Path : {self.file_path}")
+            print(f"{vbr_signature}")
+            sys.exit(1)
+        
+        print("\n# Partition : NTFS - VBR")
+
+        bpb_fields = struct.unpack("<HBH5sB18sQQQB3sB3sQI", bpb_data)
+        
+        bytes_per_sector = bpb_fields[0]
+        sectors_per_cluster = bpb_fields[1]
+        reserved_sectors_count = bpb_fields[2]
+        reserved_1 = bpb_fields[3]
+        media_type = bpb_fields[4]
+        reserved_2 = bpb_fields[5]
+        total_sectors = bpb_fields[6]
+        start_cluster_for_mft = bpb_fields[7]
+        start_cluster_for_mftmirr = bpb_fields[8]
+        cluster_per_entry = bpb_fields[9]
+        reserved_3 = bpb_fields[10]
+        clusters_per_index = bpb_fields[11]
+        reserved_4 = bpb_fields[12]
+        volume_serial_number = bpb_fields[13]
+        reserved_5 = bpb_fields[14]
+
+        print(f"[ + ] Jump Boot Code : {" ".join(f"{byte:02X}" for byte in jump_boot_code)}")
+        print(f"[ + ] OEM ID ( STR / HEX ) : {oem_id.decode(errors='errors').strip()} / {" ".join(f"{byte:02X}" for byte in oem_id)}")
+        print(f"[ + ] Bytes Per Sector : {bytes_per_sector} ( Byte )")
+        print(f"[ + ] Sectors Per Cluster : {sectors_per_cluster} ( Sector )")
+        print(f"    ( ... )")
+        print(f"[ + ] Start Cluster for $MFT : {start_cluster_for_mft} ( Sector )")
+        print(f"[ + ] Start Cluster for $MFTMirr : {start_cluster_for_mftmirr} ( Sector )")
+        print(f"    ( ... )")
+
+        self.bytes_per_sector = bytes_per_sector
+        self.sectors_per_cluster = sectors_per_cluster
+
+        self.partition_start_sector = partition_starting_offset
+        self.partition_start_offset = self.partition_start_sector * self.bytes_per_sector
+        self.mft_start_sector = partition_starting_offset + start_cluster_for_mft * self.sectors_per_cluster
+        self.mft_start_offset = self.mft_start_sector * self.bytes_per_sector
+
+        return True
+
+    """
+    NTFS - MFT Entries - MFT Entry - $MFT
+    """
+    def get_mft_entries_count(self, f) :
+        f.seek(self.mft_start_offset)
+
+        mft_entry_size = 1024
+
+        mft_data = f.read(mft_entry_size)
+
+        # MFT Entry Header - $MFT
+        mft_entry_header_data = mft_data[0:48]
+
+        mft_entry_header_signature = mft_entry_header_data[0:4]
+        offset_of_fixup_array = mft_entry_header_data[4:6]
+        entries_in_fixup_array = mft_entry_header_data[6:8]
+        log_file_sequence_number = mft_entry_header_data[8:16]
+        sequence_number = mft_entry_header_data[16:18]
+        hard_link_count = mft_entry_header_data[18:20]
+        offset_of_file_attribute = mft_entry_header_data[20:22]
+        flags = mft_entry_header_data[22:24]
+        real_size_of_mft_entry = mft_entry_header_data[24:28]
+        allocated_size_of_mft_entry = mft_entry_header_data[28:32]
+        file_reference_to_base_entry = mft_entry_header_data[32:40]
+        next_attribute_id = mft_entry_header_data[40:42]
+        allign_to_4b_boundary = mft_entry_header_data[42:44]
+        mft_entry_number = mft_entry_header_data[44:48]
+
+        if mft_entry_header_signature != MFT_ENTRY_HEADER_SIGNATURE :
+            print("\n[ ERROR ] FAIL - MFT Entry Header Signature ( FILE )")
+            print(f">>>>>>>>>> File Path : {self.file_path}")
+            print(f"{mft_entry_header_signature.decode(errors='ignore')}")
+            sys.exit(1)
+        
+        # MFT Entry Header - $MFT - Attribute Header
+        attribute_offset = int.from_bytes(offset_of_file_attribute, byteorder='little')
+
+        while attribute_offset < mft_entry_size :
+            # print(f"[ DEBUG ] Attribute Offset : {attribute_offset}")
+
+            attribute_header_data = mft_data[attribute_offset:attribute_offset+16]
+
+            attribute_type_id = int.from_bytes(attribute_header_data[0:4], byteorder='little')
+            length_of_attribute = attribute_header_data[4:8]
+            non_resident_flag = attribute_header_data[8:9]
+            length_of_name = attribute_header_data[9:10]
+            offset_to_name = attribute_header_data[10:12]
+            flags = attribute_header_data[12:14]
+            attribute_identifier = attribute_header_data[14:16]
+
+            # End
+            if attribute_type_id == 0xFFFFFFFF :
+                break
+
+            # $DATA
+            if attribute_type_id == 0x80 :
+                non_resident_flag = mft_data[attribute_offset+0x08:attribute_offset+0x08+1]
+
+                # Resident
+                if non_resident_flag == 0 :
+                    mft_size = int.from_bytes(mft_data[attribute_offset+0x10:attribute_offset+0x10+4], byteorder='little') # Resident - Size of $MFT
+                
+                # Non-Resident
+                else :
+                    mft_size = int.from_bytes(mft_data[attribute_offset+0x30:attribute_offset+0x30+8], byteorder='little') # Non-Resident - Size of $MFT
+            
+                mft_entries_count = mft_size // mft_entry_size
+
+                return mft_entries_count
+            
+            # print(f"[ DEBUG ] Length of Attribute : {length_of_attribute}")
+            
+            attribute_offset += int.from_bytes(length_of_attribute, byteorder='little')
+        
+        return 0
+
+    """
+    NTFS - MFT Entries - MFT Entry
+    """
+    def get_mft_entry(self, f) :
+        print()
+    
+    """
+    NTFS - MFT Entries
+    """
+    def get_mft_entries(self, f) :
+        print("\n# Partition : NTFS - MFT Entries")
+        print(f">>>> MFT ( Start ) Sector : {self.mft_start_sector} ( Sector )")
+
+        mft_entry_size = 1024
+
+        mft_entries_count = self.get_mft_entries_count(f)
+
+        print(f">>>> Total Number of MFT Entries : {mft_entries_count}")
+
+        # MFT Entry
+        for index in range(mft_entries_count) :
+            f.seek(self.mft_start_offset + (mft_entry_size * index))
+
+            mft_entry = f.read(mft_entry_size)
+
+            # End
+            if len(mft_entry) < mft_entry_size :
+                break
+
+            # MFT Entry - MFT Entry Header
+            mft_entry_header_signature = mft_entry[0:4]
+            offset_of_fixup_array = mft_entry[4:6]
+            entries_in_fixup_array = mft_entry[6:8]
+            log_file_sequence_number = mft_entry[8:16]
+            sequence_number = mft_entry[16:18]
+            hard_link_count = mft_entry[18:20]
+            offset_of_file_attribute = mft_entry[20:22]
+            mft_entry_flags = mft_entry[22:24]
+            real_size_of_mft_entry = mft_entry[24:28]
+            allocated_size_of_mft_entry = mft_entry[28:32]
+            file_reference_to_base_entry = mft_entry[32:40]
+            next_attribute_id = mft_entry[40:42]
+            allign_to_4b_boundary = mft_entry[42:44]
+            mft_entry_number = mft_entry[44:48]
+
+            if mft_entry_header_signature != MFT_ENTRY_HEADER_SIGNATURE :
+                continue
+
+            # MFT Entry - Attribute Header
+            attribute_offset = int.from_bytes(offset_of_file_attribute, byteorder='little')
+
+            while attribute_offset < mft_entry_size :
+                # print(f"[ DEBUG ] Attribute Offset : {attribute_offset}")
+
+                attribute_header_data = mft_entry[attribute_offset:attribute_offset+16]
+
+                attribute_type_id = int.from_bytes(attribute_header_data[0:4], byteorder='little')
+                length_of_attribute = int.from_bytes(attribute_header_data[4:8], byteorder='little')
+                non_resident_flag = int.from_bytes(attribute_header_data[8:9], byteorder='little')
+                length_of_name = int.from_bytes(attribute_header_data[9:10], byteorder='little')
+                offset_to_name = int.from_bytes(attribute_header_data[10:12], byteorder='little')
+                attribute_flags = attribute_header_data[12:14]
+                attribute_identifier = attribute_header_data[14:16]
+
+                # End
+                if attribute_type_id == 0xFFFFFFFF :
+                    break
+
+                # $FILE_NAME
+                if attribute_type_id == 0x30 :
+                                        
+                    # MFT Entry - Attribute Header - Resident
+                    if non_resident_flag == 0 :
+                        # print("[ DEBUG ] Resident")
+
+                        resident_attribute_header_data = mft_entry[attribute_offset+16:attribute_offset+32]
+
+                        length_of_attribute_content = resident_attribute_header_data[0:4]
+                        offset_to_attribute_data = int.from_bytes(resident_attribute_header_data[4:6], byteorder='little')
+                        indexed_flag = resident_attribute_header_data[6:7]
+                        reserved = resident_attribute_header_data[7:8]
+                        attribute_name = resident_attribute_header_data[8:16]
+
+                        file_name_length_offset = attribute_offset + offset_to_attribute_data + 0x40
+                        file_name_length = int.from_bytes(mft_entry[file_name_length_offset:file_name_length_offset+1], byteorder='little')
+
+                        if file_name_length == 0:
+                            attribute_offset += length_of_attribute
+
+                            continue                            
+
+                        file_name_offset = attribute_offset + offset_to_attribute_data + 0x42
+                        file_name_bytes = mft_entry[file_name_offset : file_name_offset + (file_name_length * 2)]
+                        file_name = file_name_bytes.decode('utf-16-le', errors='ignore').strip("\x00")
+
+                    # MFT Entry - Attribute Header - Non-Resident => [ To - Do ]
+                    else :
+                        # print("[ DEBUG ] Non-Resident")
+
+                        file_name = ""
+
+                    self.file_list.append(file_name)
+
+                attribute_offset += length_of_attribute
+        
+        print("\n# Partition : NTFS - File List")
+        print(f"{self.file_list}")
+        print(f">>>> Total Number of Files : {len(self.file_list)}")
+    
+    """
     NTFS
     """
     def get_ntfs(self, f) :
         print("\n# NTFS")
 
+        is_ntfs = False
+
         print("\n========================================")
+
+        for index, partition in enumerate(self.partition_list) :
+            is_ntfs = self.get_vbr(f, partition)
+
+            if is_ntfs == True :
+                self.get_mft_entries(f)
+            else :
+                continue
 
         print("\n========================================")
             
     """
-    GPT_NTFS
+    NTFS
     """
     def parse(self) :
         print("Program Start.")
